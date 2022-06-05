fada8cc3647bf5738b72038accf8b0e2755b33d4
Hidenari <hidenari.miwa@mac.com>
2022-06-05 15:52:26 +0900

    analysis by pyspark part1

--- database/pkl/hw_sum.py
+++ database/pkl/hw_sum.py
@@ -11,15 +11,16 @@ from typing import Dict
 データ範囲に発売日が含まれていないものは累計データから取り除く必要がある｡
 データ内の各ゲーム機の発売日状況
 
+- GB  1989/4/21 NG
+- N64  1996/6/23 NG
+- PS  1994/12/3 NG
+- SATURN  1994/11/22 NG
+
 - DS  2004/12/2
 - DC  1998/11/27 OK ここがギリギリ
-- GB  1989/4/21 NG
 - GBA  2001/3/21 OK
-- N64  1996/6/23 NG
 - NeoGeoP 1998/10/28 OK  ここが本当のギリギリ
-- PS  1994/12/3 NG
 - WS  1999/3/4 OK
-- SATURN  1994/11/22 NG
 - Xbox 2001/11/15 OK
 - PKS 1999/1/23 OK
 - PSP 2002/12/12 OK
@@ -37,27 +38,27 @@ def make_sumcum():
 
 def make_launchsum():
     launch = {
-        "Switch": datetime.datetime(2017, 3, 3),
         "PS5": datetime.datetime(2020, 11, 12),
+        'XSX':datetime.datetime(2020, 11, 10),
+        "Switch": datetime.datetime(2017, 3, 3),
+        'XBOne':datetime.datetime(2014, 9, 4),
         "PS4": datetime.datetime(2014, 2, 22),
         "WiiU": datetime.datetime(2012,11,18),
-        "3DS": datetime.datetime(2011,2,26),
-        'DC': datetime.datetime(1998, 11, 27),
-        'DS':datetime.datetime(2004, 12, 2),
-        'GBA':datetime.datetime(2001, 3, 21),
-        'GC':datetime.datetime(2001, 9, 14),
-        'NeoGeoP':datetime.datetime(1998, 10, 28),
-        'PS2':datetime.datetime(2000, 3, 4),
-        'PS3':datetime.datetime(2006, 11, 11),
-        'PSP':datetime.datetime(2002, 12, 12),
-        'WS':datetime.datetime(1999, 3, 4),
         'Vita':datetime.datetime(2011, 12, 17),
+        "3DS": datetime.datetime(2011,2,26),
         'Wii':datetime.datetime(2006, 12, 2),
+        'PS3':datetime.datetime(2006, 11, 11),
         'XB360':datetime.datetime(2005, 12, 10),
-        'XBOne':datetime.datetime(2014, 9, 4),
-        'XSX':datetime.datetime(2020, 11, 10),
+        'DS':datetime.datetime(2004, 12, 2),
+        'PSP':datetime.datetime(2002, 12, 12),
         'Xbox':datetime.datetime(2001, 11, 15),
-        'PKS':datetime.datetime(1999, 1, 23)
+        'GC':datetime.datetime(2001, 9, 14),
+        'GBA':datetime.datetime(2001, 3, 21),
+        'PS2':datetime.datetime(2000, 3, 4),
+        'WS':datetime.datetime(1999, 3, 4),
+        'PKS':datetime.datetime(1999, 1, 23),
+        'DC': datetime.datetime(1998, 11, 27),
+        'NeoGeoP':datetime.datetime(1998, 10, 28),
         }
     
     hw_sum = pd.read_pickle("weekly_hard_cumsum.pkl")
