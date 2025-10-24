2d5b5136b249c337ccb52a3180a094679a36ac81
Hidenari <hidenari.miwa@mac.com>
2025-10-24 12:44:50 +0900

    Report 2025.10.19

--- notebook/analysis/report_config.py
+++ notebook/analysis/report_config.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
 def get_config() -> dict:
-    the_date = datetime(2025, 10, 12)
+    the_date = datetime(2025, 10, 19)
     date_str = the_date.strftime("%Y.%m.%d")
     
     return {
         "date": the_date,
         "large": True,
-        "description": f"嵐の前の静けさ:{date_str}ハード週販レポート",
+        "description": f"ZAでSwitch2が大躍進:{date_str}ハード週販レポート",
     }
