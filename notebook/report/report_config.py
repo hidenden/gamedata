75a52454779ab035429dcc60c4471b31a9b2404c
Hidenari <hidenari.miwa@mac.com>
2026-04-17 12:46:11 +0900

    Report 2026-04-12

--- notebook/report/report_config.py
+++ notebook/report/report_config.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
 def get_config() -> dict:
-    the_date = datetime(2026, 4, 5)
+    the_date = datetime(2026, 4, 12)
     date_str = the_date.strftime("%Y.%m.%d")
     
     return {
         "date": the_date,
         "year": the_date.year,
         "large": True,
-        "description": f"Switch2国内累計500万台突破 PS4集計終了:{date_str}ハード週販レポート",
+        "description": f"Q2はハード販売台数控えめ:{date_str}ハード週販レポート",
     }
