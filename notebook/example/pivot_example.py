c0f9cd36304376a4ff9e172a4b506fb153c5b89f
Hidenari <hidenari.miwa@mac.com>
2026-04-21 20:03:01 +0900

    pivot_example.pyの冗長コード修正

--- notebook/example/pivot_example.py
+++ notebook/example/pivot_example.py
@@ -32,13 +32,8 @@ def _():
 @app.cell
 def _(g, pl):
     df_all: pl.DataFrame = g.load_hard_sales()
-    return (df_all,)
-
-
-@app.cell
-def _(df_all: "pl.DataFrame", g):
     hw_list = g.get_hw(df_all)
-    return (hw_list,)
+    return df_all, hw_list
 
 
 @app.cell(hide_code=True)
@@ -51,19 +46,17 @@ def _(mo):
 
 @app.cell
 def _(hw_list, mo):
-    hw_options = hw_list
-    hw_multiselect = mo.ui.multiselect(options=hw_options, 
+    hw_multiselect = mo.ui.multiselect(options=hw_list, 
         value=['NSW', "PS5", "NS2"], label="ハードウェアを選択してください")
     return (hw_multiselect,)
 
 
 @app.cell
-def _(date, hw_multiselect, mo):
+def _(date, mo):
     begin_date = mo.ui.date(label="開始日", value=date(2017,3,3))
     end_date = mo.ui.date(label="終了日")
-    left = mo.hstack([begin_date, end_date], justify="start",align="stretch")
-    mo.vstack([left, hw_multiselect], justify="start", align="stretch")
-    return begin_date, end_date
+    date_select = mo.hstack([begin_date, end_date], justify="start",align="stretch")
+    return begin_date, date_select, end_date
 
 
 @app.cell
@@ -75,8 +68,17 @@ def _(mo):
 
 
 @app.cell
-def _(begin_date, df_all: "pl.DataFrame", end_date, g, hw_multiselect):
-    g.pivot_sales(df_all, begin=begin_date.value, end=end_date.value, hw=hw_multiselect.value)
+def _(
+    begin_date,
+    date_select,
+    df_all: "pl.DataFrame",
+    end_date,
+    g,
+    hw_multiselect,
+    mo,
+):
+    _df = g.pivot_sales(df_all, begin=begin_date.value, end=end_date.value, hw=hw_multiselect.value)
+    mo.vstack([date_select, hw_multiselect, _df], justify="start", align="stretch")
     return
 
 
@@ -131,6 +133,14 @@ def _(mo):
 
 
 @app.cell
+def _(mo):
+    mode_dropdown = mo.ui.dropdown(options = ["week", "month", "year"], 
+        value="week", label="集計単位を選択してください")
+    mode_dropdown
+    return (mode_dropdown,)
+
+
+@app.cell
 def _(
     begin_date,
     df_all: "pl.DataFrame",
@@ -144,20 +154,21 @@ def _(
     return
 
 
-@app.cell
-def _(mo):
-    mode_dropdown = mo.ui.dropdown(options = ["week", "month", "year"], 
-        value="week", label="集計単位を選択してください")
-    mode_dropdown
-    return (mode_dropdown,)
-
-
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
     ## pivot_sales_by_delta
     """)
     return
+
+
+@app.cell
+def _(mo):
+    delta_begin = mo.ui.number(start=0, label="経過期間の最小値", value=0)
+    delta_end = mo.ui.number(start=1, label="経過期間の最大値", value=52)
+
+    mo.vstack([delta_begin, delta_end], justify="start", align="stretch")
+    return delta_begin, delta_end
 
 
 @app.cell
@@ -174,15 +185,6 @@ def _(
         hw=hw_multiselect.value,
         mode=mode_dropdown.value)
     return
-
-
-@app.cell
-def _(mo):
-    delta_begin = mo.ui.number(start=0, label="経過期間の最小値", value=0)
-    delta_end = mo.ui.number(start=1, label="経過期間の最大値", value=52)
-
-    mo.vstack([delta_begin, delta_end], justify="start", align="stretch")
-    return delta_begin, delta_end
 
 
 @app.cell(hide_code=True)
@@ -222,7 +224,6 @@ def _(begin_date, df_all: "pl.DataFrame", end_date, g):
     g.pivot_maker(df_all, 
         begin_year=begin_date.value.year, 
         end_year=end_date.value.year)
-
     return
 
 
@@ -235,44 +236,38 @@ def _(mo):
 
 
 @app.cell
-def _(hw_list, mo):
-    hw_periods_num : int = 3
-    hw_periods_ui = []
+def _(date, hw_list, mo):
+    _hwnum : int = 3
+    _init_hw = ["NSW", "NS2", "PS5"]
+    _init_date = [date(2017,3,3), 
+        date(2025,6,5), date(2020,11,15)]
 
-    for i in range(hw_periods_num):
-        ui_elements = {}    # HWを選択するdropdown
-        ui_elements["hw_dropdown"] = mo.ui.dropdown(options=hw_list, label=f"{i+1}番目のハードウェア")
-        # 集計開始日の選択
-        ui_elements["begin_date"] = mo.ui.date(label=f"{i+1}番目の集計開始日")
-        hw_periods_ui.append(ui_elements)
-
+    hw_periods_ui = mo.ui.array([
+        mo.ui.array([
+            mo.ui.dropdown(options=hw_list, label=f"{_i+1}番目のハードウェア", value=_init_hw[_i]),
+            mo.ui.date(label=f"{_i+1}番目の集計開始日", value=_init_date[_i])
+        ]) for _i in range(_hwnum)
+    ])
     period_end = mo.ui.number(start=1, label="期間数", value=52)
 
-    ui_items = [mo.hstack([ui["hw_dropdown"], ui["begin_date"]], justify="start", align="stretch") for ui in hw_periods_ui]
+    ui_items  = [mo.hstack(_hwui, justify="start") for _hwui in hw_periods_ui]
     ui_items.append(period_end)
-    mo.vstack(
-        ui_items,
-        justify="start", align="stretch"
-    )
-    return hw_periods_ui, period_end
+
+    return hw_periods_ui, period_end, ui_items
 
 
 @app.cell
-def _(df_all: "pl.DataFrame", g, hw_periods_ui, period_end):
-    hw_periods = []
+def _(df_all: "pl.DataFrame", g, hw_periods_ui, mo, period_end, ui_items):
+    hw_periods = [
+        {
+            "hw": _ui[0],
+            "begin": _ui[1],
+            "label": f"{_ui[0]} {_ui[1]}"    
+        } for _ui in hw_periods_ui.value
+    ]
 
-    for ii, ui in enumerate(hw_periods_ui):
-        hw = ui["hw_dropdown"].value
-        bd = ui["begin_date"].value
-        lv = f"{hw} {bd}"
-        period_element = {
-            "hw": hw,
-            "begin": bd,
-            "label": lv    
-        }
-        hw_periods.append(period_element)
-
-    g.pivot_sales_with_offset(df_all, hw_periods=hw_periods, end=period_end.value)
+    _df = g.pivot_sales_with_offset(df_all, hw_periods=hw_periods, end=period_end.value)
+    mo.vstack(items=[*ui_items , _df])
     return
 
 
@@ -286,9 +281,8 @@ def _(mo):
 
 @app.cell
 def _(df_all: "pl.DataFrame", g):
-    cmplist = [("NSW", "PS4"), ("NS2", "PS5")]
-    g.cumsum_diffs(df_all, cmplist=cmplist)
-
+    _cmplist = [("NSW", "PS4"), ("NS2", "PS5")]
+    g.cumsum_diffs(df_all, cmplist=_cmplist)
     return
 
 
