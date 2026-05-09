f6ab45d496eec46b4df229b3d91b724937f00975
Hidenari <hidenari.miwa@mac.com>
2026-05-09 09:23:48 +0900

    #230 use chart_hbar_maker_share_by_year()

--- notebook/draft/issue230_altChartShare.py
+++ notebook/draft/issue230_altChartShare.py
@@ -30,8 +30,8 @@ def _(df_all):
     return
 
 
-app._unparsable_cell(
-    r"""
+@app.cell
+def _(df_all):
     def chart_hbar_maker_share_by_year(
         begin:datetime | date | None = None,
         end: datetime | date | None = None
@@ -53,24 +53,32 @@ app._unparsable_cell(
         _base = alt.Chart(_df).encode(
             y=alt.Y('year:O', sort='descending', title='年'),
             x=alt.X('yearly_pct:Q', stack='normalize', title='メーカーシェア'),
-            color=alt.Color('maker_name:N',
-            scale=alt.Scale(domain=maker_list, range=maker_color),
-        ),
-        order=alt.Order('mid_point:Q'),
-    )
+            color=alt.Color('maker_name:N', title='メーカー',
+                            scale=alt.Scale(domain=maker_list, range=maker_color)),
+            order=alt.Order('mid_point:Q'),
+        )
 
-    _bars = _base.mark_bar()
-    _text = _base.mark_text(size=12, baseline='middle').encode(
-        detail='maker_name:N',
-        color=alt.value('white'),
-        text=alt.Text('pct_label:N'),
-        x=alt.X('mid_point:Q'),
-    )
-    return (_bars + _text).properties(width=800, height=400, title='年間シェア推移')
+        _bars = _base.mark_bar()
+        _text = _base.mark_text(size=12, baseline='middle').encode(
+            detail='maker_name:N',
+            color=alt.value('white'),
+            text=alt.Text('pct_label:N'),
+            x=alt.X('mid_point:Q'),
+        )
+        return (_bars + _text).properties(width=800, height=400, title='年間シェア推移')
 
-    """,
-    name="_"
-)
+
+    return
+
+
+@app.cell
+def _():
+    c1 = g.chart_hbar_maker_share_by_year(
+        datetime(2016, 1, 1),
+        datetime(2026, 12, 31)
+    )
+    mo.ui.altair_chart(c1)
+    return
 
 
 if __name__ == "__main__":
